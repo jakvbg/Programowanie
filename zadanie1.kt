9e8a0249d50918c6a1645b89f1a26c763b7b2aab
jakvbg <164903251+jakvbg@users.noreply.github.com>
2024-04-06 21:27:41 +0200

    Update zadanie1.kt

--- zadanie1.kt
+++ zadanie1.kt
@@ -2,12 +2,19 @@ package com.example.lista1
 
 import kotlin.math.sqrt
 
+/*
+@Author: Jakub Główka
+Funkcja wykorzystuje wzór Herona do obliczenia pola trójkąta.
+Args:
+    a, b, c (Int) - boki trójkąta
+Returns:
+    (Double) - pole trójkąta
+ */
 fun heron(a: Int, b: Int, c: Int): Double {
     require(a > 0 && b > 0 && c > 0){"Podaj dlugosci bokow wieksze niz zero."}
     if (a + b < c || b + c < a || c + a < b){
+        //Sprawdzenie warunku na istnienie trójkąta
         println("Trojkat o podanych bokach nie istnieje.")
-    }
-    else{
     }
     val p = (a + b + c)*0.5
     return sqrt(p*(p - a)*(p - b)*(p - c))
